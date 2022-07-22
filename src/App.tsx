709c798374ae309fb8c724f179c2dbdba89e8334
Tamara Organ <tamara@organ.dev.br>
2022-07-22 10:07:48 -0300

    add styled components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from 'styled-components'
+import { GlobalStyle } from './styles/global'
+import { defaultTheme } from './styles/theme/default'
+
 function App() {
-   return <div className="App">Hello</div>;
+  return (
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
+      <div>Hello</div>
+    </ThemeProvider>
+  )
 }
 
-export { App };
+export { App }
